fef3ecba6b7662d35072f40f55ab9f5cda632e4f
carmo-ga <ga.karmo@gmail.com>
2020-09-03 23:29:25 +0000

    03/09/20 Busca sequencial

---
+++ busca-sequencial2.js
@@ -0,0 +1,28 @@
+function buscaSequencial(lista, valorBusca, fnComp) {
+    for(let i = 0; i < lista.length; i++) {
+        //Encontrou o que está buscando: retorna a posição em que foi encontrado
+        //if(lista[i] === valorBusca) return i
+        if(fnComp(lista[i], valorBusca)) return i
+    }
+    return -1 // valorBusca não foi encontrado em lista
+}
+
+const listaNomes = require('./dados/lista-nomes')
+
+function comparaNome(obj, nome) {
+    // Retorna true se o atributo first_name do objeto for igual a nome
+    return obj.first_name === nome
+}
+
+console.time('FAUSTO')
+//console.log(buscaSequencial(listaNomes, 'FAUSTO', comparaNome))
+console.log(buscaSequencial(listaNomes, 'FAUSTO', (obj, nome) => obj.first_name === nome))
+console.timeEnd('FAUSTO')
+
+console.log('------------------')
+
+console.time('10000+')
+// Encontrar o primeiro nome da lista que possui mais de 10000 pessoas registradas com ele
+let posicao = buscaSequencial(listaNomes, 1000000, (obj, valor) => obj.frequency_total > valor)
+console.log(listaNomes[posicao], posicao)
+console.timeEnd('10000+')
