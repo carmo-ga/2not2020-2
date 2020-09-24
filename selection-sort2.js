7eb3402ea06c6494459aed1989bc1c10714b5d94
carmo-ga <ga.karmo@gmail.com>
2020-09-24 23:35:22 +0000

    24/09/20 Finalização Selection Sort

--- selection-sort2.js
+++ selection-sort2.js
@@ -1,21 +1,25 @@
-// Aula 18/09/2020
-
-
-function selectionSort(vetor) {
+// Aula 24/09/2020
+/*
+    Função de comparação fnComp
+    - Recebe dois valores para comparação
+    - Retorno:
+        * true se o PRIMEIRO valor for MAIOR que o segundo
+        * false caso contrário
+*/
+function selectionSort(vetor, fnComp) {
     let passadas = 0, comparacoes = 0, totalTrocas = 0
-
 
     // Função que encontra o menor valor em um segmento de vetor (subvetor)
     // A função deve retornar A POSIÇÃO do menor valor encontrado
     function encontrarMenor(vetor, inicio) {
         let posMenor = inicio
         for(let i = inicio + 1; i < vetor.length; i++) {
-            if(vetor[i] < vetor[posMenor]) posMenor = i
+            //if(vetor[i] < vetor[posMenor]) posMenor = i
+            if(! fnComp(vetor[i], vetor[posMenor])) posMenor = i
             comparacoes++
         }
         return posMenor
     }
-
 
     // O for externo vai até a PENÚLTIMA posição
     for(i = 0; i < vetor.length - 1; i++) {
@@ -26,7 +30,8 @@ function selectionSort(vetor) {
 
         // Caso o valor encontrado seja menor que o valor atual, procede-se a troca
         comparacoes++
-        if(vetor[posMenor] < vetor[i]) {
+        //if(vetor[posMenor] < vetor[i]) {
+        if(fnComp(vetor[i], vetor[posMenor])) { // Ordem dos argumentos invertida
             // Permuta de valores usando desestruturação
             [vetor[posMenor], vetor[i]] = [vetor[i], vetor[posMenor]]
             totalTrocas++
@@ -34,25 +39,16 @@ function selectionSort(vetor) {
     }
     console.log({passadas, comparacoes, totalTrocas})
 }
-/*
-const nums = [56, 78, 44, 23, 99, 14, 60, 37, 6, 82, 31, 65]
 
-//console.log(encontrarMenor(nums, 3))  // Retornar 8 (posição do valor 6)
 
-console.time('Time')
-selectionSort(nums)
-console.timeEnd('Time')
 
-console.log(nums)
-*/
-
-let nomes = require('./dados/100-mil-nomes')
+let candidatos = require('./dados/candidatos-2018')
 console.time('Nomes')
-selectionSort(nomes)
+selectionSort(candidatos, (x, y) => x.NM_CANDIDATO > y.NM_CANDIDATO)
 console.timeEnd('Nomes')
 
 // Medindo a memória utilizada pelo programa
 // Retorna a quantidade de memória em bites (kilobyte e megabyte)
 let memoria = process.memoryUsage().heapUsed / 1024 / 1024
-console.log(nomes)
+console.log(candidatos)
 console.log('Memória utilizada (MB): ', memoria)
